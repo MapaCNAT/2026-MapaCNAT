fbc12dee283bfd6a25f3f7405b9f8110ccedd760
Isaque-Ias <gabriel.isaias.gomes.silva@gmail.com>
2026-05-26 19:36:32 -0300

    chore: corrigir chave

--- mapa_cnat/settings.py
+++ mapa_cnat/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tk&-i3kn-^kzy0^ka)bvbq^m_bd^4922y+w+u0v8tqt#_0mr!2'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'chave-insegura')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
