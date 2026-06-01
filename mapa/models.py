396109dace077f689a1d46010f95643a3da22db4
Isaque-Ias <gabriel.isaias.gomes.silva@gmail.com>
2026-06-01 20:12:25 -0300

    bugfix: Remodelar

--- mapa/models.py
+++ mapa/models.py
@@ -1,35 +1,6 @@
 from django.db import models
 
-class Local(models.Model):
-    nome = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.nome
-
-class Construcao(Local):
-    andares = models.IntegerField()
-
-class Juncao(Local):
-    coordenadas = models.CharField(max_length=255)
-    vizinhos = models.ManyToManyField('self', blank=True)
-
-class Amigo(models.Model):
-    nome = models.CharField(max_length=255)
-
-    def coordenadas(self):
-        pass
-
-    def __str__(self):
-        return self.nome
-
-class Piso(models.Model):
-    planta = models.CharField(max_length=255)
-    construcao = models.ForeignKey(Construcao, on_delete=models.CASCADE, related_name='pisos')
-
 class Banner(models.Model):
-    piso = models.OneToOneField(Piso, on_delete=models.CASCADE, related_name='banner')
-    local = models.OneToOneField(Local, on_delete=models.CASCADE, related_name='banner_local')
-
     def gerar_html(self):
         pass
 
@@ -42,15 +13,31 @@ class BlocoBanner(models.Model):
     def __str__(self):
         return self.titulo
 
-class Buscador(models.Model):
-    amigos = models.ManyToManyField(Amigo, related_name='buscadores')
-    locais = models.ManyToManyField(Local, related_name='buscadores')
+class Juncao(models.Model):
+    coordenadas = models.CharField(max_length=255)
+    vizinhos = models.ManyToManyField('self', blank=True)
 
-    def gerar_rota(self, local_a, local_b):
+class Local(models.Model):
+    nome = models.CharField(max_length=255)
+    juncao = models.ForeignKey(Juncao, on_delete=models.CASCADE, related_name='locais')
+    banner = models.OneToOneField(Banner, on_delete=models.CASCADE, related_name='local')
+
+    def __str__(self):
+        return self.nome
+
+class Construcao(models.Model):
+    local = models.OneToOneField(Local, on_delete=models.CASCADE, related_name='construcao')
+
+class Amigo(models.Model):
+    nome = models.CharField(max_length=255)
+
+    def coordenadas(self):
         pass
 
-    def buscar_local(self, nome):
-        return Local.objects.filter(nome__icontains=nome)
-
-    def buscar_amigo(self, nome):
-        return Amigo.objects.filter(nome__icontains=nome)
+    def __str__(self):
+        return self.nome
+    
+class Piso(models.Model):
+    planta = models.CharField(max_length=255)
+    construcao = models.ForeignKey(Construcao, on_delete=models.CASCADE, related_name='pisos')
+    banner = models.OneToOneField(Banner, on_delete=models.CASCADE, related_name='piso')
