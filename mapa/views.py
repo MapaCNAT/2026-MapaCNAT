31d432b49bd4c09b4ae05473215f5df1ba57f057
Isaque-Ias <gabriel.isaias.gomes.silva@gmail.com>
2026-06-17 20:25:36 -0300

    style: Pagina inicial e estrutura de templates

--- mapa/views.py
+++ mapa/views.py
@@ -1,13 +1,13 @@
 from django.http import HttpResponse
 from django.template import loader
-from django.shortcuts import redirect
 
 def mapa(request):
     template = loader.get_template('mapa.html')
     return HttpResponse(template.render())
 
 def home(request):
-    return redirect("login")
+    template = loader.get_template('home.html')
+    return HttpResponse(template.render())
 
 def login(request):
     template = loader.get_template('login.html')
